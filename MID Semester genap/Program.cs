28f5850c16f149de6cf0df7ea0dc2a183abd807f
Auliyarois <roizfatoni290@gmail.com>
2022-05-18 23:27:16 +0700

    inisialisasi project

---
+++ MID Semester genap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MID_Semester_genap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen(1, "Kamarudin, M.Kom", 21114457, "Laki-laki", "Pemrograman");
+
+            dosen1.cetakHasil();
+            Console.WriteLine();
+            dosen1.addDosen();
+            dosen1.addCourse();
+            Console.WriteLine();
+            dosen1.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
